2bb2dff1049acfcae8cf575734c21f32a5c65d9c
Dee Newcum <dee.newcum@gmail.com>
2015-02-24 13:59:18 -0600

    use the fix for Google Chrome suggested at http://stackoverflow.com/a/6825715/1042525

--- reddit-allow-use-of-cloudsearch-syntax.user.js
+++ reddit-allow-use-of-cloudsearch-syntax.user.js
@@ -1,4 +1,4 @@
-// ==UserScript==
+﻿// ==UserScript==
 // @name        reddit: Allow use of cloudsearch syntax
 // @author      Sophie Hamilton (/u/Sophira)
 // @description Adds a checkbox to allow the use of cloudsearch syntax with the reddit search widget, and provides a link to /u/interiot's cloudsearch syntax page.
@@ -12,66 +12,74 @@
 // @grant       none
 // ==/UserScript==
 
-(function() {
-  // add the cloudsearch checkbox
-  var restricts = document.querySelectorAll('form#search div#moresearchinfo');
-  for (var i = 0; i < restricts.length; i++) {
-    var checked="false";
-    var qs = new QueryString();
-    var cs = qs.value("syntax");
-    if (cs == "cloudsearch") {
-      checked = " checked";
-    }
 
-    var div = document.createElement("div");
-    div.innerHTML = '<label><input name="syntax" value="cloudsearch" type="checkbox"' + checked + '>use cloudsearch syntax</label> (<a href="https://cdn.rawgit.com/DeeNewcum/reddit/master/cloudsearch/cloudsearch_reference.html">help</a>)';   // thanks to /u/interiot for the wonderful page on cloudsearch syntax!
-    var info = restricts.item(i);
-    var parent = info.parentNode;
-    parent.insertBefore(div, info);
 
-    // because some of the #moresearchinfo styles only trigger if there's a LABEL directly before it, we need to add those back manually. Hax :(
-    // in the case of the search not being on the sidebar, we also need to check to see if any BRs need to be added for layout purposes.
-    info.style.borderTopWidth = "1px";
-    if (parent.id == "searchexpando") {   // sidebar
-      info.style.marginTop = "0px";
-    }
-    else {
-      var br = document.querySelector("form#search br");
-      if (!br) {
-        // add a couple
-        parent.insertBefore(document.createElement("br"), div);
-        parent.insertBefore(document.createElement("br"), div);
+// Google Chrome doesn't support @require in Greasemonkey scripts.
+// So this is a workaround.  http://stackoverflow.com/a/6825715/1042525
+var load,execute,loadAndExecute;load=function(a,b,c){var d;d=document.createElement("script"),d.setAttribute("src",a),b!=null&&d.addEventListener("load",b),c!=null&&d.addEventListener("error",c),document.body.appendChild(d);return d},execute=function(a){var b,c;typeof a=="function"?b="("+a+")();":b=a,c=document.createElement("script"),c.textContent=b,document.body.appendChild(c);return c},loadAndExecute=function(a,b){return load(a,function(){return execute(b)})};
+
+
+loadAndExecute("//rawgit.com/Sophira/greasemonkey-scripts/master/resources/query-string-parser.js",
+  function() {
+    // add the cloudsearch checkbox
+    var restricts = document.querySelectorAll('form#search div#moresearchinfo');
+    for (var i = 0; i < restricts.length; i++) {
+      var checked="false";
+      var qs = new QueryString();
+      var cs = qs.value("syntax");
+      if (cs == "cloudsearch") {
+        checked = " checked";
+      }
+
+      var div = document.createElement("div");
+      div.innerHTML = '<label><input name="syntax" value="cloudsearch" type="checkbox"' + checked + '>use cloudsearch syntax</label> (<a href="https://cdn.rawgit.com/DeeNewcum/reddit/master/cloudsearch/cloudsearch_reference.html">help</a>)';   // thanks to /u/interiot for the wonderful page on cloudsearch syntax!
+      var info = restricts.item(i);
+      var parent = info.parentNode;
+      parent.insertBefore(div, info);
+
+      // because some of the #moresearchinfo styles only trigger if there's a LABEL directly before it, we need to add those back manually. Hax :(
+      // in the case of the search not being on the sidebar, we also need to check to see if any BRs need to be added for layout purposes.
+      info.style.borderTopWidth = "1px";
+      if (parent.id == "searchexpando") {   // sidebar
+        info.style.marginTop = "0px";
+      }
+      else {
+        var br = document.querySelector("form#search br");
+        if (!br) {
+          // add a couple
+          parent.insertBefore(document.createElement("br"), div);
+          parent.insertBefore(document.createElement("br"), div);
+        }
+      }
+
+      // we also need to fix the tabindex
+      var form = parent;
+      if (parent.id == "searchexpando") { form = parent.parentNode; }   // sidebar
+
+      var el;
+      el = form.querySelector('input[name="restrict_sr"]');
+      if (!el) { el = parent.querySelector('input[name="q"]'); }
+      if (el) {
+        var tabnum = parseInt(el.tabIndex);
+
+        var tabbedelements = document.querySelectorAll("*[tabindex]");
+        for (var j = 0; j < tabbedelements.length; j++) {
+          var tabbedel = tabbedelements.item(j);
+          if (tabbedel.tabIndex > tabnum) { tabbedel.tabIndex++; }
+        }
+
+        div.querySelector("input").tabIndex = tabnum + 1;
       }
     }
 
-    // we also need to fix the tabindex
-    var form = parent;
-    if (parent.id == "searchexpando") { form = parent.parentNode; }   // sidebar
-
-    var el;
-    el = form.querySelector('input[name="restrict_sr"]');
-    if (!el) { el = parent.querySelector('input[name="q"]'); }
-    if (el) {
-      var tabnum = parseInt(el.tabIndex);
-
-      var tabbedelements = document.querySelectorAll("*[tabindex]");
-      for (var j = 0; j < tabbedelements.length; j++) {
-        var tabbedel = tabbedelements.item(j);
-        if (tabbedel.tabIndex > tabnum) { tabbedel.tabIndex++; }
+    // The error message:
+    //    I couldn't understand your query, so I simplified it and ...
+    // is misleading.  Replace it with something more clear.
+    var errormsg = document.querySelector("div.infobar div.md > p");
+    if (errormsg && cs == "cloudsearch") {
+      var results = errormsg.innerHTML.match(/^I couldn't understand your query, so I simplified it and searched for "(.*)" instead\./);
+      if (results) {
+        errormsg.innerHTML = 'Cloudsearch syntax error. (<a href="https://cdn.rawgit.com/DeeNewcum/reddit/master/cloudsearch/cloudsearch_reference.html">Need help?</a>) The non-Cloudsearch search results for "' + results[1] + '" are below, but that\'s probably not what you wanted.';
       }
-
-      div.querySelector("input").tabIndex = tabnum + 1;
     }
-  }
-
-  // The error message:
-  //    I couldn't understand your query, so I simplified it and ...
-  // is misleading.  Replace it with something more clear.
-  var errormsg = document.querySelector("div.infobar div.md > p");
-  if (errormsg && cs == "cloudsearch") {
-    var results = errormsg.innerHTML.match(/^I couldn't understand your query, so I simplified it and searched for "(.*)" instead\./);
-    if (results) {
-      errormsg.innerHTML = 'Cloudsearch syntax error. (<a href="https://cdn.rawgit.com/DeeNewcum/reddit/master/cloudsearch/cloudsearch_reference.html">Need help?</a>) The non-Cloudsearch search results for "' + results[1] + '" are below, but that\'s probably not what you wanted.';
-    }
-  }
-})();
+  });
